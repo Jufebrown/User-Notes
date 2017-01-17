5db5bfd88d8a7c9b34348310d54a7202f31e938f
Jufebrown <jufebrown@gmail.com>
2017-01-17 09:53:53 -0600

    added firebase authentication

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBZDzD-r0dDyfjqypYbUgXKmebAGKzFhwk",
+  authDomain: "user-notes-74f00.firebaseapp.com",
+  databaseURL: "https://user-notes-74f00.firebaseio.com",
+  storageBucket: "user-notes-74f00.appspot.com",
+  messagingSenderId: "397241484919"
+};
+firebase.initializeApp(config);
+
 var app = angular.module('notesApp', ['ngRoute'])
 
 app.config(($routeProvider, $locationProvider) => {
