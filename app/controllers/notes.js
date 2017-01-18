72e92330850e5e76a87fc090a7fd9c44d6e13618
Jufebrown <jufebrown@gmail.com>
2017-01-18 16:03:25 -0600

    changed name of NotesCtrl

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,4 +1,4 @@
-app.controller('rt1Ctrl', function($scope) {
+app.controller('NotesCtrl', function($scope) {
   $scope.picAddress = "http://cdn.fishki.net/upload/post/201407/18/1286305/004.jpg"
   $scope.highwayName = 'US Route 1'
 })
