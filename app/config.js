1dc23fcc779a73e2392b871037ccb89678121cea
Jufebrown <jufebrown@gmail.com>
2017-01-18 16:01:58 -0600

    modified config

--- app/config.js
+++ app/config.js
@@ -29,5 +29,20 @@ app.config(($routeProvider, $locationProvider) => {
     controller: 'NewCtrl',
     templateUrl: 'partials/new.html'
   })
+  // .when("/account", {
+  //   // the rest is the same for ui-router and ngRoute...
+  //   controller: "AccountCtrl",
+  //   templateUrl: "views/account.html",
+  //   resolve: {
+  //     console.log('account')
+  //     // controller will not be loaded until $requireSignIn resolves
+  //     // Auth refers to our $firebaseAuth wrapper in the factory below
+  //     "currentAuth": ["Auth", function(Auth) {
+  //       // $requireSignIn returns a promise so the resolve waits for it to complete
+  //       // If the promise is rejected, it will throw a $routeChangeError (see above)
+  //       return Auth.$requireSignIn();
+  //     }]
+  //   }
+  // })
   .otherwise({redirectTo:'/'});
 })
