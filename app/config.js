15a171a6d621e957b8ffdb4a6461e7b4339cb1ac
Jufebrown <jufebrown@gmail.com>
2017-01-18 16:01:35 -0600

    modified config

--- app/config.js
+++ app/config.js
@@ -1,19 +1,27 @@
 app.config(($routeProvider, $locationProvider) => {
   $locationProvider.hashPrefix('')
   $routeProvider
+  // .when('/', {
+  //   controller: 'SplashCtrl',
+  //   templateUrl: 'partials/splash.html',
+  //   resolve: {
+  //     // controller will not be loaded until $waitForSignIn resolves
+  //     // Auth refers to our $firebaseAuth wrapper in the factory below
+  //     "currentAuth": ["Auth", function(Auth) {
+  //       // $waitForSignIn returns a promise so the resolve waits for it to complete
+  //       return Auth.$waitForSignIn();
+  //     }]
+  //   }
+  // })
+  // .when('/login', {
+  //   controller: 'LoginCtrl',
+  //   templateUrl: 'partials/login.html'
+  // })
+  // .when('/register', {
+  //   controller: 'RegisterCtrl',
+  //   templateUrl: 'partials/register.html'
+  // })
   .when('/', {
-    controller: 'SplashCtrl',
-    templateUrl: 'partials/splash.html'
-  })
-  .when('/login', {
-    controller: 'LoginCtrl',
-    templateUrl: 'partials/login.html'
-  })
-  .when('/register', {
-    controller: 'RegisterCtrl',
-    templateUrl: 'partials/register.html'
-  })
-  .when('/notes', {
     controller: 'NotesCtrl',
     templateUrl: 'partials/notes.html'
   })
