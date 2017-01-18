ef96e12f5e17316a52cce2a5fbe51c8161f141b7
Jufebrown <jufebrown@gmail.com>
2017-01-18 16:03:48 -0600

    changed name of SplashCtrl

--- app/controllers/splash.js
+++ app/controllers/splash.js
@@ -1,4 +1,3 @@
-app.controller('rt1Ctrl', function($scope) {
-  $scope.picAddress = "http://cdn.fishki.net/upload/post/201407/18/1286305/004.jpg"
-  $scope.highwayName = 'US Route 1'
+app.controller('SplashCtrl', function() {
+
 })
